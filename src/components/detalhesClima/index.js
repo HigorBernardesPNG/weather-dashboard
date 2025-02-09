b4f3543d48e279eed1db1c05087bdda1c4ff8702
Higor <zdtshark@gmail.com>
2025-02-09 14:23:54 -0400

    enviando

--- src/components/detalhesClima/index.js
+++ src/components/detalhesClima/index.js
@@ -1,4 +1,5 @@
 import React from 'react'; // Importa a biblioteca React
+import { ceuLimpo, nublado, chuva, Chuvisco, Tempestade, Neve, Nevoa, Neblina, Nevoeiro, Fumaca, Poeira, Areia, Cinzas, RajadasDeVento, Tornado } from '../svgClimas/index';
 import './detalhesClima.css'; // Importa o arquivo de estilo
 
 // Componente funcional DetalhesClima que recebe os dados climáticos como prop
@@ -14,26 +15,37 @@ function DetalhesClima({ dadosClima }) {
       <p className='detalhes-clima_Umidade'>💧 Umidade: {dadosClima.main.humidity}%</p> {/* Exibe a umidade */}
   
       <div className='container__iconNuvem'>
-        {dadosClima.weather[0].main === "Rain" && (
-          <svg xmlns="http://www.w3.org/2000/svg" width="88" height="88" fill="currentColor" className="bi bi-cloud-rain-fill" viewBox="0 0 16 16">
-            <path d="M4.5 14a.5.5 0 0 1-.374-.832l1.5-1.5a.5.5 0 1 1 .748.664L5 13h1.5a.5.5 0 0 1 0 1h-2zm4 0a.5.5 0 0 1-.374-.832l1.5-1.5a.5.5 0 1 1 .748.664L9 13h1.5a.5.5 0 0 1 0 1h-2zm4 0a.5.5 0 0 1-.374-.832l1.5-1.5a.5.5 0 1 1 .748.664L13 13h1.5a.5.5 0 0 1 0 1h-2z"/>
-            <path d="M11.473 9a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 14h8.5a2.5 2.5 0 1 0-.027-5"/>
-          </svg>
-        )}
-  
-        {dadosClima.weather[0].main === "Clouds" && (
-          <svg xmlns="http://www.w3.org/2000/svg" width="88" height="88" fill="currentColor" className="bi bi-clouds-fill" viewBox="0 0 16 16">
-            <path d="M11.473 9a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 14h8.5a2.5 2.5 0 1 0-.027-5"/>
-            <path d="M14.544 9.772a3.5 3.5 0 0 0-2.225-1.676 5.5 5.5 0 0 0-6.337-4.002 4.002 4.002 0 0 1 7.392.91 2.5 2.5 0 0 1 1.17 4.769z"/>
-          </svg>
-        )}
-  
-        {dadosClima.weather[0].main === "Clear" && (
-          <svg xmlns="http://www.w3.org/2000/svg" width="88" height="88" fill="currentColor" className="bi bi-brightness-high-fill" viewBox="0 0 16 16">
-            <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0z"/>
-            <path d="M8.5 0h-1v3h1V0zM8.5 13h-1v3h1v-3zM3.636 3.636l-.707.707 2.121 2.122.707-.707-2.121-2.122zm9.192 9.192l-.707.707-2.122-2.121.707-.707 2.122 2.121zM0 8.5v-1h3v1H0zm13 0v-1h3v1h-3zM3.636 12.364l.707.707 2.121-2.121-.707-.707-2.121 2.121zm9.192-9.192l.707-.707-2.121-2.122-.707.707 2.121 2.122z"/>
-          </svg>
-        )}
+
+        {dadosClima.weather[0].main === "Clear" && ceuLimpo()}
+
+        {dadosClima.weather[0].main === "Clouds" && nublado()}
+    
+        {dadosClima.weather[0].main === "Rain" && chuva()}
+
+        {dadosClima.weather[0].main === "Drizzle" && Chuvisco()}
+
+        {dadosClima.weather[0].main === "Thunderstorm" && Tempestade()}
+
+        {dadosClima.weather[0].main === "Snow" && Neve()}
+
+        {dadosClima.weather[0].main === "Mist" && Nevoa()}
+
+        {dadosClima.weather[0].main === "Haze" && Neblina()}
+
+        {dadosClima.weather[0].main === "Fog" && Nevoeiro()}
+
+        {dadosClima.weather[0].main === "Smoke" && Fumaca()}
+
+        {dadosClima.weather[0].main === "Dust" && Poeira()}
+
+        {dadosClima.weather[0].main === "Sand" && Areia()}
+
+        {dadosClima.weather[0].main === "Ash" && Cinzas()}
+
+        {dadosClima.weather[0].main === "Squall" && RajadasDeVento()}
+
+        {dadosClima.weather[0].main === "Tornado" && Tornado()}
+
       </div>
     </div>
   );
